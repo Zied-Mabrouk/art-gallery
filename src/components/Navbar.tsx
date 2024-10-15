9d1b309770c447ca39b2d7af01687ca1e557ba21
Zied Mabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2024-10-15 03:34:26 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,6 @@ const Navbar = () => {
   useEffect(() => {
     closeShowOverview();
     setActiveIllustration(null);
-    console.log('changed');
   }, [router, closeShowOverview, setActiveIllustration]);
 
   const handlers = useSwipeable({
